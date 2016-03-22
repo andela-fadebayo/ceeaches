d6011ff701b26c8a66658ad60cec0b622fa7dbde
andela-fadebayo <fiyinfoluwa.adebayo@andela.com>
2016-03-22 13:35:29 +0100

    AR-026 Message when fetching recipe.

--- app/assets/javascripts/controllers/recipe_controller.js
+++ app/assets/javascripts/controllers/recipe_controller.js
@@ -1,8 +1,10 @@
 angular.module('controllers')
   .controller('RecipeController', ['$scope', '$stateParams', 'recipeService', function ($scope, $stateParams, recipeService) {
     $scope.recipeId = $stateParams.id;
+    $scope.recipeLoading = true;
 
     recipeService.getRecipe($scope.recipeId).success(function (data) {
       $scope.recipe = data;
+      $scope.recipeLoading = false;
     });
   }]);
